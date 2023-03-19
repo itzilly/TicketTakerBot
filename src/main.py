999982147676178a26b3f8950cb09db0ed03a532
illyum <itzillyum@gmail.com>
2023-03-19 09:48:09 -0600

    Add simple startup

--- src/main.py
+++ src/main.py
@@ -1,5 +1,28 @@
+import os
+import json
+import discord
+from discord.ext import commands
+
+
+CONFIG_FILE_PATH = 'config.json'
+
+
 def main():
-	pass
+	if not os.path.exists(CONFIG_FILE_PATH) and not os.path.isfile(CONFIG_FILE_PATH):
+		with open(CONFIG_FILE_PATH, 'w') as config_file:
+			json.dump({'token': ''}, config_file, indent=2)
+
+	with open('config.json', 'r') as config_file:
+		token = json.load(config_file)['token']
+
+	intents = discord.Intents.default()
+	client = commands.Bot(intents=intents, command_prefix="!")
+
+	client.run(token=token)
+
+	@client.event
+	async def on_ready(bot):
+		print("Logged in!")
 
 
 if __name__ == '__main__':
